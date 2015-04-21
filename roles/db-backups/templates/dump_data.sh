67a1130c1e6818ed65f6710f930eec2956b75fc9
Max F. Albrecht <1@178.is>
2015-04-21 12:52:08 +0200

    lint: remove trailing whitespace
    - magic oneliner: `find . -not \( -name .svn -prune -o -name .git -prune \) -type f -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*$//"`
    - `git diff -w` is empty
    - of course the submodules are still unlinted

--- roles/db-backups/templates/dump_data.sh
+++ roles/db-backups/templates/dump_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Ansible managed 
+# Ansible managed
 
 DUMP_DIR="{{madek_root_dir}}/db/media_files/{{rails_env}}/backups"
 DUMP_FILE_NAME="madek-data-dump_$(date +%FT%T%Z).pgbin"
@@ -8,6 +8,6 @@ DUMP_FILE="${DUMP_DIR}/${DUMP_FILE_NAME}"
 LINK_NAME="latest-data.pgbin"
 LINK="${DUMP_DIR}/${LINK_NAME}"
 mkdir -p ${DUMP_DIR}
-pg_dump -F c -a -T schema_migrations -x -O -f ${DUMP_FILE} madek_{{rails_env}} 
+pg_dump -F c -a -T schema_migrations -x -O -f ${DUMP_FILE} madek_{{rails_env}}
 find ${DUMP_DIR} -name 'madek-data-dump*pgbin' -type f -mtime +10 -exec rm {} \;
 cd ${DUMP_DIR} && ln -s -f ${DUMP_FILE_NAME} ${LINK_NAME}
