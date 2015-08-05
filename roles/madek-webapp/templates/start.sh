ebbad1d3dd44cad849c10be8d6972e7a7db6cc77
Thomas Schank <DrTom@schank.ch>
2015-08-05 13:16:01 +0200

    Overhaul, deploy API

--- roles/madek-webapp/templates/start.sh
+++ roles/madek-webapp/templates/start.sh
@@ -5,7 +5,7 @@ source /etc/profile.d/rbenv-load.sh \
 && rbenv shell {{rubies.jruby.version}} \
 && export JVM_OPTS="-Xmx{{madek_webapp_xmx_value}}" \
 && export RAILS_ENV={{rails_env}}  \
-&& export RAILS_RELATIVE_URL_ROOT="{{madek_webapp_context}}" \
+&& export RAILS_RELATIVE_URL_ROOT="{{madek_web_context_or_empty}}" \
 && export MADEK_ROOT_DIR="{{madek_root_dir}}" \
 && bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:{{madek_webapp_port}} \
   >> {{madek_webapp_log_dir}}/{{madek_webapp_service_name}}.log 2>&1
