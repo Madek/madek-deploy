9e9c03726e96636049bf3ed4344f1aa7c08e5e83
Thomas Schank <DrTom@schank.ch>
2015-11-06 21:39:20 +0100

    Configure and improve memory settings

--- roles/lein-service/templates/start.sh
+++ roles/lein-service/templates/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd {{lein_service_app_dir}} \
-&& export JVM_OPTS="-Xmx{{lein_service_xmx_value}}" \
-&& lein with-profile production trampoline run >> {{lein_service_log_dir}}/console.log 2>&1
+set -eux
+cd {{lein_service_app_dir}}
+export JVM_OPTS="-Xmx{{lein_service_xmx_value}}"
+lein with-profile production trampoline run \
+    >> {{lein_service_log_dir}}/console.log 2>&1
--- roles/madek-webapp/templates/start.sh
+++ roles/madek-webapp/templates/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-source /etc/profile.d/rbenv-load.sh \
-&& cd {{madek_webapp_dir}} \
-&& rbenv-load \
-&& rbenv shell {{rubies.jruby.version}} \
-&& export JVM_OPTS="-Xmx{{madek_webapp_xmx_value}}" \
-&& export RAILS_ENV={{rails_env}}  \
-&& export RAILS_RELATIVE_URL_ROOT="{{madek_web_context_or_empty}}" \
-&& export MADEK_ROOT_DIR="{{madek_root_dir}}" \
-&& bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:{{madek_webapp_port}} \
+set -eux
+source /etc/profile.d/rbenv-load.sh
+cd {{madek_webapp_dir}}
+rbenv-load
+rbenv shell {{rubies.jruby.version}}
+
+# Rails Settings
+export RAILS_ENV={{rails_env}}
+export export RAILS_RELATIVE_URL_ROOT="{{madek_web_context_or_empty}}"
+export MADEK_ROOT_DIR="{{madek_root_dir}}"
+
+# Memory Settings
+export JVM_OPTS="-Xmx{{webapp_xmx_value}}"
+# it seems that the above is ignored,
+# we could either invoke `jruby -J-Xmx4G` or set JRUBY_OPTS
+export JRUBY_OPTS="-J-Xms{{webapp_xmx_value}} -J-Xmx{{webapp_xmx_value}}"
+
+bundle exec puma config.ru -t 4:40 -b tcp://127.0.0.1:{{madek_webapp_port}} \
   >> {{madek_webapp_log_dir}}/{{madek_webapp_service_name}}.log 2>&1
