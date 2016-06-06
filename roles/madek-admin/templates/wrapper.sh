dc1746a6af3ca8719b2772ce4f356bd02197a975
Thomas Schank <DrTom@schank.ch>
2016-06-06 15:33:21 +0200

    Stop madek processes more thoroughly, clean-up some stuff

--- roles/madek-admin/templates/wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p {{madek_admin_log_dir}}
-chown -R {{madek_user}} {{madek_admin_log_dir}}
-sudo -u {{madek_user}} -n -i {{madek_admin_service_bin}}
--- roles/madek-webapp/templates/wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p {{madek_webapp_log_dir}}
-chown -R {{madek_user}} {{madek_webapp_log_dir}}
-sudo -u {{madek_user}} -n -i {{madek_webapp_service_bin}}
