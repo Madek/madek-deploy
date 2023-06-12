12ab9520dfb0fa6297214c65cc14cfa175ff56b7
Thomas Schank <DrTom@schank.ch>
2023-06-12 09:27:50 +0200

    Fix db_dump script

--- roles/db-backups/templates/dump.sh
+++ roles/db-backups/templates/dump.sh
@@ -8,6 +8,6 @@ DUMP_FILE="${DUMP_DIR}/${DUMP_FILE_NAME}"
 LINK_NAME="latest.pgbin"
 LINK="${DUMP_DIR}/${LINK_NAME}"
 mkdir -p ${DUMP_DIR}
-/usr/lib/postgresql/15/bin/pg_dump -d 5415 -F c -x -O -f ${DUMP_FILE} madek
+/usr/lib/postgresql/15/bin/pg_dump -d madek --port 5415 -F c -x -O -f ${DUMP_FILE} 
 find ${DUMP_DIR} -name 'madek-dump*pgbin' -type f -mtime +10 -exec rm {} \;
 cd ${DUMP_DIR} && ln -s -f ${DUMP_FILE_NAME} ${LINK_NAME}
